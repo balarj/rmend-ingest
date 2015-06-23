deb3c313424e7f27cc00aa77fe9407fcefb0c02e
Bala Rajagopal <bxr4261@rit.edu>
2015-06-22 20:26:31 -0700

    Added the IRMendDao interface to the mix

---
+++ src/main/java/com/brajagopal/rmend/dao/IRMendDao.java
@@ -0,0 +1,11 @@
+package com.brajagopal.rmend.dao;
+
+import com.brajagopal.rmend.data.beans.DocumentBean;
+
+/**
+ * @author <bxr4261>
+ */
+public interface IRMendDao {
+
+    public void putDocument(DocumentBean _docBean);
+}
