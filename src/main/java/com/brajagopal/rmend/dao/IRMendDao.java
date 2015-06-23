60d329a25108a4a286d954fd8f5d479dc1c5c24c
Bala Rajagopal <bxr4261@rit.edu>
2015-06-23 04:37:28 -0700

    Refactored the IRmendDao interface to include putDocument() and getDocument() methods

--- src/main/java/com/brajagopal/rmend/dao/IRMendDao.java
+++ src/main/java/com/brajagopal/rmend/dao/IRMendDao.java
@@ -1,11 +1,15 @@
 package com.brajagopal.rmend.dao;
 
 import com.brajagopal.rmend.data.beans.DocumentBean;
+import com.google.api.services.datastore.client.DatastoreException;
 
 /**
  * @author <bxr4261>
  */
 public interface IRMendDao {
 
-    public void putDocument(DocumentBean _docBean);
+    public void putDocument(DocumentBean _docBean) throws DatastoreException;
+    public void putDocument(DocumentBean _docBean, String _identifier) throws DatastoreException;
+
+    public void getDocument(Long _documentNumber) throws DatastoreException;
 }
