d675d39fbf12faa6a0f002f47fa4f6a6b6140d52
Bala Rajagopal <bxr4261@rit.edu>
2015-06-25 05:57:46 -0700

    Registered the DocumentMetaSerDe with the JsonUtils class.

--- src/main/java/com/brajagopal/rmend/utils/JsonUtils.java
+++ src/main/java/com/brajagopal/rmend/utils/JsonUtils.java
@@ -1,6 +1,7 @@
 package com.brajagopal.rmend.utils;
 
 import com.brajagopal.rmend.data.beans.DocumentBean;
+import com.brajagopal.rmend.data.meta.DocumentMeta;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -15,6 +16,7 @@ public class JsonUtils {
         // Configure GSON
         final GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(DocumentBean.class, new DocumentBean.DocumentSerDe());
+        gsonBuilder.registerTypeAdapter(DocumentMeta.class, new DocumentMeta.DocumentMetaSerDe());
         gsonInstance = gsonBuilder.create();
     }
 
