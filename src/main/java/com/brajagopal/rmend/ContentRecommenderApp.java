2f5882a9da0d69b6944d5f0705d2fde5e6d0b168
Bala Rajagopal <bxr4261@rit.edu>
2015-06-30 23:08:25 -0700

    Added the test getContentByTopic invocations to the ContentRecommenderApp class.

--- src/main/java/com/brajagopal/rmend/ContentRecommenderApp.java
+++ src/main/java/com/brajagopal/rmend/ContentRecommenderApp.java
@@ -4,6 +4,7 @@ import com.brajagopal.rmend.dao.GCloudDao;
 import com.brajagopal.rmend.dao.IRMendDao;
 import com.brajagopal.rmend.data.meta.DocumentMeta;
 import com.brajagopal.rmend.exception.DatastoreExceptionManager;
+import com.brajagopal.rmend.recommender.ContentRecommender;
 import com.google.api.services.datastore.client.DatastoreException;
 import com.google.common.collect.TreeMultimap;
 import org.apache.log4j.Logger;
@@ -19,18 +20,31 @@ public class ContentRecommenderApp {
 
     private static Logger logger = Logger.getLogger(ContentProcessor.class);
     private static IRMendDao dao;
+    private ContentRecommender contentRecommender;
 
     private DatastoreExceptionManager datastoreExceptionManager = new DatastoreExceptionManager();
 
     public ContentRecommenderApp() throws GeneralSecurityException, IOException {
         dao = GCloudDao.getLocalInstance();
+        contentRecommender = new ContentRecommender(dao);
+    }
+
+    public ContentRecommender getRecommender() {
+        return contentRecommender;
     }
 
     public static void main(String[] args) throws DatastoreException, GeneralSecurityException, IOException {
         ContentRecommenderApp app = new ContentRecommenderApp();
-        logger.info(dao.getDocument(2330894543l).getContentBeansByType());
-        TreeMultimap<String, DocumentMeta> entityValues = dao.getEntityMeta(Arrays.asList("ENTITIES:City:athens"));
+        //logger.info(dao.getDocument(2330894543l).getContentBeansByType());
+        TreeMultimap<String, DocumentMeta> entityValues = dao.getEntityMeta(Arrays.asList("ENTITIES:Person:joachim_johansson"));
         logger.info(entityValues);
-        logger.info(dao.getDocument(2330894543l).getRelevantBeans());
+        logger.info(dao.getDocument(2423267037l).getRelevantBeans());
+        try {
+            logger.info(app.getRecommender().getContentByTopic(ContentRecommender.makeTopicBean("human_interest"), ContentRecommender.ResultsType.RANDOM_10));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 }
