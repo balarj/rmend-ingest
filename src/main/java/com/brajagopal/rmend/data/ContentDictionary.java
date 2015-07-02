84c3c09dcf7ff523f8a1bfe2134cbd322486685e
Bala Rajagopal <bxr4261@rit.edu>
2015-07-01 23:58:25 -0700

    Added the makeBeanFromKey() method to the ContentDictionary class.

--- src/main/java/com/brajagopal/rmend/data/ContentDictionary.java
+++ src/main/java/com/brajagopal/rmend/data/ContentDictionary.java
@@ -9,12 +9,13 @@ import com.google.api.services.datastore.client.DatastoreException;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.collections4.list.TreeList;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.io.InvalidClassException;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author <bxr4261>
@@ -37,20 +38,20 @@ public class ContentDictionary {
                 continue;
             }
 
-            String key = makeKey(_contentBean);
+            String key = makeKeyFromBean(_contentBean);
             dict.put(key, DocumentMeta.createInstance(_documentBean.getDocumentNumber(), _documentBean.getDocId(), _contentBean.getScore()));
         }
     }
 
-    public Collection<DocumentMeta> getData(BaseContent.ContentType _contentType, String _type, String _name) {
+    /*public Collection<DocumentMeta> getData(BaseContent.ContentType _contentType, String _type, String _name) {
         Collection<DocumentMeta> retVal;
         String key = _contentType + KEY_SEPARATOR + _type  + KEY_SEPARATOR + _name;
         StringUtils.replace(key, KEY_SEPARATOR+KEY_SEPARATOR, KEY_SEPARATOR);
         retVal = new TreeList<DocumentMeta>(dict.get(key));
         return retVal;
-    }
+    }*/
 
-    public static String makeKey(BaseContent _contentBean) {
+    public static String makeKeyFromBean(BaseContent _contentBean) {
 
         String beanType = "";
         String beanName = "";
@@ -65,6 +66,19 @@ public class ContentDictionary {
         catch (UnsupportedOperationException e) {}
 
         return StringUtils.join(Arrays.asList(_contentBean.getContentType(), beanType, beanName), KEY_SEPARATOR);
+    }
+
+    public static BaseContent makeBeanFromKey(String _key) throws IllegalAccessException, InvalidClassException, InstantiationException {
+        String[] keyToken = _key.split(KEY_SEPARATOR);
+        Map<String, String> tokens = new HashMap<String, String>(3);
+        if (keyToken.length != 3) {
+            throw new UnsupportedOperationException("The key '"+_key+"' is not a valid one.");
+        }
+        tokens.put("contentType", keyToken[0]);
+        tokens.put("type", keyToken[1]);
+        tokens.put("name", keyToken[2]);
+        return BaseContent.getChildInstance(tokens);
+
     }
 
     public static BaseContent.ContentType getContentType(String _key) {
